a00e56bd6bc2ea33b7275065fc1bd2cdcd7f6b90
otte <90667972+Constanted313@users.noreply.github.com>
2024-01-01 00:48:39 +0500

    Update MD5-A.py

--- MD5-A.py
+++ MD5-A.py
@@ -18,11 +18,10 @@ ENDC   = "\u001b[0m"
 def ec(): print(ENDC, end='')
 
 s = 0
+blockedsymbols = ['/', '\\', ':', '?', '<', '>', '|']
 def IgnoreObjSet(InputFolder):
     global IgnoreFilesize
     global IgnoreFolders
-
-    blockedsymbols = ['/', '\\', ':', '?', '<', '>', '|']
 
     InputFolder = InputFolder.split("*")
 
@@ -40,8 +39,35 @@ def IgnoreObjSet(InputFolder):
 
     except: print(f"{' ' * s}   $ {RED}Вводите вес в числовом виде{ENDC}")
 
+def ClearIgnore(IgnoreFolders):
+    global IgnoreFilesize
+
+    IgnoreFolders.clear()
+    IgnoreFilesize = 99 ** 99
+
+    print(f"   $ {ORANGE}Список игнорируемых папок и игнорируемый размер файлов очищены.{ENDC}")
+
+
+def CheckIgnore(filefolder):
+    global Ignored
+
+    filefolder_splitted = filefolder.split("\\")
+
+    i=0
+    while i < len(filefolder_splitted):
+
+        if filefolder_splitted[i] in IgnoreFolders:
+
+            Ignored = True
+            i = 0; break
+        
+        else: i += 1
+
+
 
 def MD5_Calculate(InputFolder):
+    global IgnoreFilesize
+    global Ignored
 
     t_start = time()
 
@@ -50,6 +76,8 @@ def MD5_Calculate(InputFolder):
     f_errors = 0
     
     md5log   = ''
+
+    i = 0
     
     for filefolder, dirs, files in walk(InputFolder):
         for filename in files:
@@ -61,34 +89,26 @@ def MD5_Calculate(InputFolder):
             fullpath    = f"{filefolder}\\{filename}"
             filesize_mb = path.getsize(fullpath) / (1024*1024)
 
-            if len(IgnoreFolders) > 0:
-                filefolder_splitted = filefolder.split("\\")
-                i = 0
+            if len(IgnoreFolders) > 0: CheckIgnore(filefolder)
 
-                while i < len(filefolder_splitted):
-                    if filefolder_splitted[i] in IgnoreFolders:
-                        Ignored = True
-                        i = 0; break
-                    else: i += 1
-                i = 0
 
-            try:
-                if filesize_mb < IgnoreFilesize and filesize_mb > 0 and filename != "desktop.ini" and not Ignored:
-                    
-                    TerminalWidth = get_terminal_size()[0]
-                    info = f"\r{' ' * 36}← {fullpath if not OnlyNames else filename}"
-                    if len(info) > TerminalWidth: info = info[0:TerminalWidth-3] + " .."
-                    print(info, end='')
-                    
-                    with open(fullpath, 'rb') as fp:
-                        md5_hash = calc_md5_for(fp.read()).hexdigest()
-                        md5log  += f"{fullpath if not OnlyNames else filename}\n{md5_hash}\n"
+            if filesize_mb < IgnoreFilesize and filesize_mb > 0:
+                if filename != "desktop.ini" and not Ignored:
+
+                    try:
+                        info = f"\r{' ' * 36}← {fullpath if not OnlyNames else filename}"; TerminalWidth = get_terminal_size()[0]
+                        if len(info) > TerminalWidth: info = info[0:TerminalWidth-3] + " .."
+                        print(info, end='')
                         
-                        f_calced += 1
-                        
-                        print(f"\r   {md5_hash}", flush=True)
+                        with open(fullpath, 'rb') as fp:
+                            md5_hash = calc_md5_for(fp.read()).hexdigest()
+                            md5log  += f"{fullpath if not OnlyNames else filename}\n{md5_hash}\n"
+                            
+                            f_calced += 1
+                            
+                            print(f"\r   {md5_hash}", flush=True)
 
-            except Exception as e: f_errors += 1; print(f"\r   {RED}{e}{ENDC}", flush=True)
+                    except Exception as e: f_errors += 1; print(f"\r   {RED}{e}{ENDC}", flush=True)
 
 
     t_total = time() - t_start
@@ -110,10 +130,11 @@ def MD5_Calculate(InputFolder):
         if md5l_file == "*отмена": break
         else:
             try:
+                print("   $ Сохранение файла..", end='')
                 md5l = open(md5l_file, 'w', encoding='utf-8')
                 md5l.write(md5log), md5l.close()
 
-                print(f"{ENDC}   $ {ORANGE}Файл{ENDC} {YELLOW}{path.abspath(md5l_file)}{ENDC} {ORANGE}сохранён.{ENDC}\n{'―' * TerminalWidth}")
+                print(f"{ENDC}\r   $ {ORANGE}Файл{ENDC} {YELLOW}{path.abspath(md5l_file)}{ENDC} {ORANGE}сохранён.{ENDC}\n{'―' * get_terminal_size()[0]}", flush=True)
                 break
 
             except PermissionError : print(f"   $ {RED}Ошибка создания файла{ENDC} ({YELLOW}Нет разрешения{ENDC}).")
@@ -121,6 +142,8 @@ def MD5_Calculate(InputFolder):
             except Exception as e  : print(f"   $ {RED}Ошибка создания файла{ENDC} ({YELLOW}{e}{ENDC}).")
 
 def MD5_Search(InputFolder):
+    global IgnoreFilesize
+    global Ignored
 
     t_start = time()
 
@@ -141,34 +164,41 @@ def MD5_Search(InputFolder):
     for filefolder, dirs, files in walk(InputFolder):
         for filename in files:
 
-            f_passed   += 1
+            f_passed += 1
+
+            Ignored = False
 
             fullpath    = f"{filefolder}\\{filename}"
             filesize_mb = path.getsize(fullpath) / (1024*1024)
 
-            TerminalWidth = get_terminal_size()[0]
-            searching = f"\r    $ Поиск по списку MD5 данных в выбранной папке: {f_passed}/{f_count_total} {filename if OnlyNames else fullpath}"
-            searching += ' ' * (TerminalWidth - len(searching))
-            if len(searching) > TerminalWidth: searching = searching[0:TerminalWidth-3] + " .."
-            print(searching, end='', flush=True)
+            if len(IgnoreFolders) > 0: CheckIgnore(filefolder)
 
-            try:
-                if filesize_mb < IgnoreFilesize and filesize_mb > 0 and filename != "desktop.ini":
-                    with open(fullpath, 'rb') as fp: curr_md5_hash = calc_md5_for(fp.read()).hexdigest()
 
-                    if curr_md5_hash in md5log_IMPORTED:
-                        md5log_IMPORTED_founded_md5index = md5log_IMPORTED.index(curr_md5_hash)
+            if filesize_mb < IgnoreFilesize and filesize_mb > 0:
+                if filename != "desktop.ini" and not Ignored:
+                    try:
 
-                        if md5log_IMPORTED_founded_md5index // 2 == 0:
-                            md5log_IMPORTED_founded_md5      = md5log_IMPORTED[md5log_IMPORTED_founded_md5index]
-                            md5log_IMPORTED_founded_filename = md5log_IMPORTED[md5log_IMPORTED_founded_md5index - 1]
+                        searching = f"\r    $ Поиск по списку MD5 данных в выбранной папке: {f_passed}/{f_count_total} {filename if OnlyNames else fullpath}"
+                        TerminalWidth = get_terminal_size()[0]; searching += ' ' * (TerminalWidth - len(searching))
+                        if len(searching) > TerminalWidth: searching = searching[0:TerminalWidth-3] + " .."
+                        print(searching, end='', flush=True)
 
-                            print(f"\r      {GREEN}Найден: {md5log_IMPORTED_founded_filename}{ENDC} (MD5:{md5log_IMPORTED_founded_md5}) как {YELLOW}{fullpath}{ENDC}", flush=True)
-                            
-                            f_founds += 1
+                        with open(fullpath, 'rb') as fp: curr_md5_hash = calc_md5_for(fp.read()).hexdigest()
 
-            except Exception as e: f_errors += 1; print(f"\r      {RED}Ошибка: {fullpath}{ENDC}: {YELLOW}{e}{ENDC}", flush=True)     
+                        if curr_md5_hash in md5log_IMPORTED:
+                            md5log_IMPORTED_founded_md5index = md5log_IMPORTED.index(curr_md5_hash)
 
+                            if md5log_IMPORTED_founded_md5index % 2 == 0:
+                                md5log_IMPORTED_founded_md5      = md5log_IMPORTED[md5log_IMPORTED_founded_md5index]
+                                md5log_IMPORTED_founded_filename = md5log_IMPORTED[md5log_IMPORTED_founded_md5index - 1]
+
+                                print(f"\r      {GREEN}Найден: {md5log_IMPORTED_founded_filename}{ENDC} (MD5:{md5log_IMPORTED_founded_md5}) как {YELLOW}{fullpath}{ENDC}", flush=True)
+                                
+                                f_founds += 1
+
+                    except Exception as e: f_errors += 1; print(f"\r      {RED}Ошибка: {filename}{ENDC}: {YELLOW}{e}{ENDC}", flush=True)     
+
+    TerminalWidth = get_terminal_size()[0]
     if f_founds == 0: print(f"\r{' ' * TerminalWidth}\n\r      Ничего не найдено . . .{' ' * (TerminalWidth - 29)}\n\n    $ {ORANGE}Поиск по списку MD5 данных в выбранной папке завершён.{ENDC}{' ' * (TerminalWidth - 60)}", flush=True)
     else: print(f"\r    $ {ORANGE}Поиск по списку MD5 данных в выбранной папке завершён.{ENDC}{' ' * (TerminalWidth - 60)}", flush=True)
 
@@ -204,12 +234,8 @@ if __name__ == "__main__":
                         if path.isdir(InputFolder): print(""); MD5_Calculate(InputFolder)
                         else: print(f"   $ {RED}Каталог не найден.{ENDC}")
 
-                    elif InputFolder == "*назад": break
-                    elif InputFolder == "*очистить": 
-                        IgnoreFolders.clear()
-                        IgnoreFilesize = 99 ** 99
-                        print(f"   $ {ORANGE}Список игнорируемых папок и игнорируемый размер файлов очищены.{ENDC}")
-
+                    elif InputFolder == "*назад"   : break
+                    elif InputFolder == "*очистить": ClearIgnore(IgnoreFolders)
                     else: s = 0; IgnoreObjSet(InputFolder)
 
             elif CurrentInput == op[1]:
@@ -229,14 +255,10 @@ if __name__ == "__main__":
                                 if path.isdir(InputFolder): MD5_Search(InputFolder)
                                 else: print(f"    $ {RED}Каталог не найден.{ENDC}")
 
-                            elif InputFolder == "*назад": break
-                            elif InputFolder == "*имена":  OnlyNames = True;  print(f"    $ {ORANGE}Только имена файлов.{ENDC}")
-                            elif InputFolder == "*полные": OnlyNames = False; print(f"    $ {ORANGE}Полные пути ко всем файлам.{ENDC}")
-                            elif InputFolder == "*очистить": 
-                                IgnoreFolders.clear()
-                                IgnoreFilesize = 99 ** 99
-                                print(f"   $ {ORANGE}Список игнорируемых папок и игнорируемый размер файлов очищены.{ENDC}")
-
+                            elif InputFolder == "*назад"   : break
+                            elif InputFolder == "*имена"   : OnlyNames = True;  print(f"    $ {ORANGE}Только имена файлов.{ENDC}")
+                            elif InputFolder == "*полные"  : OnlyNames = False; print(f"    $ {ORANGE}Полные пути ко всем файлам.{ENDC}")
+                            elif InputFolder == "*очистить": ClearIgnore(IgnoreFolders)
                             else: s = 1; IgnoreObjSet(InputFolder)
 
                     elif md5_data_file == "*назад": break
@@ -244,8 +266,7 @@ if __name__ == "__main__":
                 
                 OnlyNames = OnlyNamesOldState
 
-
-            elif CurrentInput == "*имена":  OnlyNames = True;  print(f"  $ {ORANGE}Только имена файлов.{ENDC}")
+            elif CurrentInput == "*имена" : OnlyNames = True;  print(f"  $ {ORANGE}Только имена файлов.{ENDC}")
             elif CurrentInput == "*полные": OnlyNames = False; print(f"  $ {ORANGE}Полные пути ко всем файлам.{ENDC}")
             
             elif CurrentInput == op[2]: ec(); exit()
